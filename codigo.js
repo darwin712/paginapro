239a16407524fdc14b065dbebc2290aa912c757b
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-03 20:49:45 -0700

    si
    si

--- codigo.js
+++ codigo.js
@@ -1,20 +1,11 @@
 
 function prueba(){
 
-/* 
-
-let numerosComoTexto = inputText.split(",");
-
-let numeros = numerosComoTexto.map(Number);
-
-let parrafo = document.getElementById('miParrafo');
-
-parrafo.innerText = numeros.join(", ");
-*/
-
 let input = document.getElementById('textarea_numeros').value;
 
 let numerosComoTexto = input.split(",");
+
+let numeros = numerosComoTexto.map(Number).join("<br>");;
 
 let parrafo = document.getElementById('miParrafo');
 
