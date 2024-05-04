4edce4994d615d07165cac9177e9c1d2060e51fc
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-03 21:00:34 -0700

    JKASDKJKJSA
    JKASKJKJDAS

--- codigo.js
+++ codigo.js
@@ -5,7 +5,21 @@ let input = document.getElementById('textarea_numeros').value;
 
 let numerosComoTexto = input.split(",");
 
-let numeros = numerosComoTexto.map(Number).join("<br>");;
+let numeros = numerosComoTexto.map(Number);
+
+var e = numeros.length;
+
+for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+        if (numeros[j] > numeros[j + 1]) {
+            var temp = numeros[j];
+            numeros[j] = numeros[j + 1];
+            numeros[j + 1] = temp;
+        }
+    }
+
+
+}
 
 let parrafo = document.getElementById('miParrafo');
 
