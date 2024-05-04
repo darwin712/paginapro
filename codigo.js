e2a6e879b7a8cac87f3b0ad37f880e5f9b341e4b
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-03 20:07:01 -0700

    ya le cambie otra vez
    si esta chido el github

--- codigo.js
+++ codigo.js
@@ -3,6 +3,4 @@ let input_text = document.getElementById('textarea_numeros').value;
 
 let nums = inputText.split(",");
 
-let parrafo = document.getElementById('miParrafo');
-
-parrafo.innerText = nums;
+console.log(nums);
