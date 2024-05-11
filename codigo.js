1f7d23abd231ff281b407678dfb1a85595d147a7
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-11 00:42:33 -0700

    si

--- codigo.js
+++ codigo.js
@@ -23,35 +23,6 @@ var media = numeros.reduce((a,b) => a + b, 0) / numeros.length;
 
 var mediana = (numeros[(numeros.length - 1) >> 1] + numeros[numeros.length >> 1]) / 2;
 
-function calcularModa() {
-    var frecuencias = {};
-    var maxFrecuencia = 0;
-    var modas = [];
-
-    // Calcular las frecuencias
-    for(var e = 0; e < numeros.length; e++) {
-        var num = numeros[e];
-        frecuencias[num] = (frecuencias[num] || 0) + 1;
-        if(frecuencias[num] > maxFrecuencia) {
-            maxFrecuencia = frecuencias[num];
-        }
-    }
-
-    // Encontrar todas las modas
-    for(var num in frecuencias) {
-        if(frecuencias[num] === maxFrecuencia) {
-            modas.push(Number(num));
-        }
-    }
-
-    var parrafo3 = document.getElementById('moda');
-
-    parrafo3.innerText = "La moda del array es: " + modas.join(", ");
-}
-
-
-var moda = numeros.sort((a,b) => numeros.filter(v => v===a).length - numeros.filter(v => v===b).length).pop();
-
 var varianza = numeros.reduce((a, b) => a + Math.pow(b - media, 2), 0) / numeros.length;
 
 var desviacion_estandar = Math.sqrt(numeros.reduce((a, b) => a + Math.pow(b - media, 2), 0) / numeros.length);
@@ -77,3 +48,19 @@ if(dato == "DVE"){
 }
 
 }
+
+let textarea = document.getElementById('textarea_numeros');
+let mensaje = document.getElementById('mensaje');
+
+textarea.addEventListener('input', function() {
+
+  if (!/^[\d,]*$/.test(this.value)) {
+ 
+    mensaje.style.display = 'block';
+    this.disabled = true;
+  } else {
+    
+    mensaje.style.display = 'none';
+    this.disabled = false;
+  }
+});
