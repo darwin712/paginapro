7eb78b73415cb4dd16f67f842e588850c102d0be
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-20 01:53:22 -0700

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -59,6 +59,37 @@ if(dato == "Moda"){
     resultModa.innerText = "Moda(s) = "+ moda;
 }
 
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Media', 'Mediana', 'Moda'],
+        datasets: [{
+            label: '# de Estadísticas',
+            data: [media, mediana, moda],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
 }
 
 }
@@ -189,34 +220,4 @@ document.addEventListener("DOMContentLoaded", () => {
             return;
         }
     });
-});
-
-let ctx = document.getElementById('myChart').getContext('2d');
-let myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Media', 'Mediana', 'Moda'],
-        datasets: [{
-            label: '# de Estadísticas',
-            data: [media, mediana, moda],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
 });
