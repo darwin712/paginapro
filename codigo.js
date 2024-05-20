f08f8d7495da073acafb2eeedcc9401064f16f89
xMrDoge5262 <eitanraultacho@gmail.com>
2024-05-20 01:20:46 -0700

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -5,7 +5,7 @@ var input = document.getElementById('textarea_numeros').value;
 
 if(input === ""){
      var result = document.getElementById("result");
-     result.inneErText  = "Resultado";
+     result.innerText = "Resultado"
 }else{
 
 var numerosComoTexto = input.split(",");
